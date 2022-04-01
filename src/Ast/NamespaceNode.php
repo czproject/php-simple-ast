46949b36103dfdfa4659b01b20b462105787e099
Jan Pecha <janpecha@email.cz>
2022-04-01 10:29:14 +0200

    AST: NamespaceNode parse PhpDocNode

--- src/Ast/NamespaceNode.php
+++ src/Ast/NamespaceNode.php
@@ -145,6 +145,9 @@
 				} elseif ($parser->isCurrent(T_COMMENT)) {
 					$child = CommentNode::parse($parser->createSubParser());
 
+				} elseif ($parser->isCurrent(T_DOC_COMMENT)) {
+					$child = PhpDocNode::parse($parser->createSubParser());
+
 				} elseif ($parser->isCurrent(T_NEW)) {
 					$parser->consumeUnknow();
 
