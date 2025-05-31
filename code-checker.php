933e00c9a5149c11963f116da6be6aa7cc857c74
Jan Pecha <janpecha@email.cz>
2025-05-31 10:56:08 +0200

    CodeChecker: updated

--- code-checker.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return function (JP\CodeChecker\CheckerConfig $config) {
-	$config->setPhpVersion(new JP\CodeChecker\Version('8.0.0'));
-	$config->addPath('./src');
-	$config->addPath('./tests');
-	$config->addIgnore('fixtures/*');
-	JP\CodeChecker\Sets\CzProjectMinimum::configure($config);
-};
